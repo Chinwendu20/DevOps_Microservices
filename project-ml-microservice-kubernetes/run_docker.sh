58760cdc799676cb59072df59e11010c491a4360
EC2 Default User <ec2-user@ip-172-31-9-57.ec2.internal>
2022-09-01 17:13:34 +0000

    Added files

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag chinwendu20/ubuntu .
+docker build --tag chinwendu20/udacity:v1 .
 
 # Step 2: 
 # List docker images
@@ -12,4 +12,4 @@ docker image ls
 
 # Step 3: 
 # Run flask app
-docker run -p 8000:80 chinwendu20/ubuntu
+docker run -p 8000:80 chinwendu20/udacity:v1
